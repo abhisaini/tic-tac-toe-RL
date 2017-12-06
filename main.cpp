c2aa8e1dbc7b1b56d0ca8955aa543eb900350f21
abhisaini <abhisni@iitk.ac.in>
2017-12-07 02:54:04 +0530

    function added to check game status at certain state

--- main.cpp
+++ main.cpp
@@ -26,3 +26,42 @@ float check(state s, std::vector<state> stateArray ){
 
 	return 0.5;
 }
+
+
+
+//function to check status of game at certain state
+int GameOver(int state[3][3])
+{
+    //checking for 3 1s or 3 -1s in a row
+    for(int i=0;i<3;i++){
+        if((state[i][1]==1)&&(state[i][2]==1)&&(state[i][3]==1)){
+            return 1;
+        }
+        
+        if((state[i][1]==-1)&&(state[i][2]==-1)&&(state[i][3]==-1)){
+            return 0;
+        }
+    }
+    
+    //checking for 3 1s or 3 -1s in a column
+    for(int i=0;i<3;i++){
+        if((state[1][i]==1)&&(state[2][i]==1)&&(state[3][i]==1)){
+            return 1;
+        }
+        
+        if((state[3][i]==-1)&&(state[2][i]==-1)&&(state[3][i]==-1)){
+            return 0;
+        }
+    }
+    //checking for 3 1s in both the diagonals and return 1 for win
+    if(((state[1][1]==1)&&(state[2][2]==1)&&(state[3][3]==1))||((state[3][1]==1)&&(state[2][2]==1)&&(state[1][3]==1))){
+        return 1;
+    }
+    //checking for 3 1s in both the  and return 0 for lose
+    if(((state[1][1]==-1)&&(state[2][2]==-1)&&(state[3][3]==-1))||((state[3][1]==-1)&&(state[2][2]==-1)&&(state[1][3]==-1))){
+        return 0;
+    }
+    
+    return checkState(); //checkState function to check the value function of the state
+    
+}    
