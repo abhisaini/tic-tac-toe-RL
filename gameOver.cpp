f0b35dc46053599615bba4496d9715411ec85c10
abhisaini <abhisni@iitk.ac.in>
2017-12-08 02:39:58 +0530

    function to check game status at certain state

--- gameOver.cpp
+++ gameOver.cpp
@@ -5,29 +5,6 @@
 
 using namespace std;
 
-struct state {
-	int mat[3][3];
-	float val;
-};
-
-bool compareMatrix(int mat1[3][3], int mat2[3][3]){
-	for (int i = 0; i < 3; i++) {
-		for (int j = 0; j < 3;)
-	}
-
-}
-
-float check(state s, std::vector<state> stateArray ){
-	for (int i = 0; i < stateArray.size(); i++){
-		if (s == stateArray[i]) {
-			return stateArray[i].val;
-		}
-	}
-
-	return 0.5;
-}
-
-
 
 //function to check status of game at certain state
 int GameOver(int state[3][3])
