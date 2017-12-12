b5edc7629010c8d1978d3a4245cb12d3a572a5f3
Akash Kumar Singh <ksakash@users.noreply.github.com>
2017-12-12 05:34:32 +0530

    Update main.cpp
    fixed indentation and fixed minor bugs

--- main.cpp
+++ main.cpp
@@ -1,5 +1,5 @@
 #include <bits/stdc++.h>
-#include "matplotlibcpp.h"  //library for plotting
+#include "matplotlibcpp.h" // library for plotting
 #define GREEDY 0
 #define EXPLORATORY 1
 #define PLAYER_X 1
@@ -10,11 +10,11 @@
 #define DRAW 4
 
 using namespace std;
-namespace plt = matplotlibcpp; //namespace for plotting
+namespace plt = matplotlibcpp; // namespace for plotting
 typedef std::vector<int> Vector;
 typedef std::vector<std::vector<int> > Matrix;
 
-class state {
+class state { // inorder to describe  a state with the positions of X and O and its value function
 public:
 	int gridSize;
 	Matrix mat;
@@ -38,11 +38,9 @@ bool compareMatrices(Matrix mat1, Matrix mat2){
 	return true;
 }
 
-std::vector<state> stateArray;
+std::vector<state> stateArray; // array to store all the states possible
 
-//function to check value of states which are not stored
-/********************************************************************************************
-**********************************************************************************************/
+// function to check value of states which are not stored in the stateArray
 float getValUnknown(Matrix arr)
 {
 	int sz = arr.size();
@@ -62,10 +60,10 @@ float getValUnknown(Matrix arr)
   }
 
   //checking for sz 1s or sz -1s in a column
-  for(int i=0;i<sz;i++){
-    int sum=0;
-  	for(int j = 0; j < sz; j++){
-			sum=sum+arr[j][i];
+  for(int i = 0; i < sz; i++){
+    int sum = 0;
+  	for (int j = 0; j < sz; j++) {
+			sum = sum + arr[j][i];
     }
     if(sum == sz){
       return 1;
@@ -73,25 +71,21 @@ float getValUnknown(Matrix arr)
     if(sum == -sz){
         return 0;
     }
+	}
+	//checking sz 1s or -1s in  diagonal
+  int sumd1 = 0, sumd2 = 0;
+  for (int i = 0; i < sz; i++) {
+		sumd1 = sumd1 + arr[i][i];
+    sumd2 = sumd2 + arr[i][sz-1-i];
+  }
+
+	if((sumd1 == sz)||(sumd2 == sz)) return 1;
+  if((sumd1 == sz)||(sumd2 == sz)) return 0;
+
+	return val;
 }
 
-
-    //checking sz 1s or -1s in  diagonal
-    int sumd1=0,sumd2=0;
-    for(int i=0;i<sz;i++){
-        sumd1=sumd1+arr[i][i];
-        sumd2=sumd2+arr[i][sz-1-i];
-    }
-    if(sumd1==sz||sumd2==sz) return 1;
-    if(sumd1==sz||sumd2==sz) return 0;
-
-
-    return val;
-}
-/**********************************************************************************************************
-***********************************************************************************************************/
-
-// checks the state array if the given state already exist or not
+// gives the value function of the function
 float getVal(state s){
 	for (int i = 0; i < stateArray.size(); i++) {
 		if (compareMatrices(s.mat, stateArray[i].mat)) {
@@ -109,16 +103,14 @@ void equate(state &state1, state &state2) {
 	}
 	state2.val = state1.val;
 }
-//the below snippet represents function randomMove(),whose input are a const state state1,a state state 2,player(1 or -1).And the function will ch
-/******************************************************************************************************************************
-********************************************************************************************************************************/
-//will return possible space at any state and takes state as input
+
+// the functions counts the positions where a player can move
 int spaceRandom(state const &s) {
-    int sz=s.mat.size();
-    int val=0;
-    for(int i=0;i<sz;i++) {
-       for(int j=0;j<sz;j++) {
-           if(s.mat[i][j]!=0) {
+    int sz = s.mat.size();
+    int val = 0;
+    for(int i = 0; i < sz; i++) {
+       for(int j = 0; j < sz; j++) {
+           if(s.mat[i][j] != 0) {
                val++;
            }
        }
@@ -128,18 +120,10 @@ int spaceRandom(state const &s) {
 
 //function will take the array containing the position of vacancies as input and returns a random vacancy
 int randomInput(int arr[], int length){
-    int a = rand();
-    int c = rand();
-    int b = rand();
-    int d = rand();
-    	d = rand();
-    	d = rand();
-    	d = rand();
-   	d = rand();
-	 	d = rand();
 
-    int input=rand()%length;
-    return arr[input];
+	// srand(time(NULL)); // can use this to decrease the training time
+  int input=rand()%length;
+  return arr[input];
 }
 
 //it will make the move i.e it takes the state,vacancy and player(1 or -1) as input and changes the vacancy to player
@@ -147,12 +131,11 @@ void randomChanged(state &S1, state &S2, int move, int player){
 
 	int sz = S1.mat.size();
 	int moveX=move/sz, moveY=move%sz;
-    equate(S1, S2);
+  equate(S1, S2);
 
-    S2.mat[moveX][moveY]= player;
+  S2.mat[moveX][moveY]= player;
   return;
 }
-
 
 void randomMove(state &S, state &S1, int player){
     int space = spaceRandom(S);
@@ -174,47 +157,35 @@ void randomMove(state &S, state &S1, int player){
     return;
 }
 
-/******************************************************************************************************************************
-********************************************************************************************************************************/
-
-
-
-//Must be placed below functions spaceRandom and getValUnknown
-//input will be the state
-//will return 1 for win,0 for loose,2 for game not over,4 for draw
-/************************************************************************************
-***************************************************************************************/
 int GameOver(state &arr){
 
     float val=getValUnknown(arr.mat);
     int vacancy=spaceRandom(arr);
-    if(vacancy==0&&val==0.5) return DRAW; //4 stands for draw
-    if(val!=0.5) return val;//will return 0 if lost and 1 if won;
+    if(vacancy==0&&val==0.5) return DRAW;
+    if(val!=0.5) return val;//will return LOSE if lost and WIN if won;
     else if(vacancy>0&&val==0.5) return 2;//2 stands for game not over yet
 
 }
+
 string OX(int a){
-	string empty=" ",xOccupied="x",oOccupied="o";
-	if (a==0) return empty;
-	if (a==1) return xOccupied;
-	if (a==-1) return oOccupied;
+	string empty = "  ", xOccupied = " x", oOccupied = " o";
+	if (a == 0) return empty;
+	if (a == 1) return xOccupied;
+	if (a == -1) return oOccupied;
 	else return to_string(a);
 }
-/******************************************************************************************************
-*****************************************************************************************************/
 
 void printMat(int gridSize, Matrix mat){
     for (int i = 0; i < gridSize; i++){
         for (int j = 0; j < gridSize; j++){
             cout << OX(mat[i][j]);
-			if(j<gridSize-1){
-				 cout<< " | ";
-			}
+						if(j<gridSize-1){
+				 			cout<< " |";
+						}
         }
-        if(i<gridSize-1) cout << "______________________" << endl;
+        if(i < gridSize - 1) cout << endl << "--------------------" << endl;
     }
-
-    cout << "-----------------------" << endl;
+    cout <<endl<< "___________________" << endl;
 }
 
 void nextMove (state &currState, state &nextState, int policy, int player){
@@ -261,7 +232,6 @@ void nextMove (state &currState, state &nextState, int policy, int player){
 		nextState.val = getVal(nextState);
 
 	}
-
 	else {
 		randomMove(currState, nextState, player);
 	}
@@ -280,7 +250,6 @@ void backUp(state &prevState, state &currState, float alpha){ // assuming prevSt
 }
 
 void pushBack(state S){
-
 	int gridSize = S.mat.size();
 	state S1(gridSize);
 	equate(S, S1);
@@ -321,24 +290,23 @@ void playGame(float epsilon, float alpha, int gridSize){ // plays a game, object
 		}
 		else {policy = GREEDY;}
 		nextMove(oState, xState, policy, PLAYER_X);
+		int gameRes = GameOver(xState);
 		if (!alreadyExist(xState)) pushBack(xState);
 		int i = getStateIndex(dummyState);
 		if (i != -1 && policy == GREEDY ) backUp(stateArray[i], xState, alpha);
-		if ((GameOver(xState) == WIN)||(GameOver(xState) == LOSE)||(GameOver(xState) == DRAW)){
+		if ((gameRes == WIN)||(gameRes == DRAW)) {
 		    if (GameOver(xState) == WIN) win++;
 		    else draw++;
 		    break;
 		}
 		equate(xState, dummyState);
-		//j++;
 		nextMove(xState, oState, EXPLORATORY, PLAYER_O);
-		if ((GameOver(oState) == WIN)||(GameOver(oState) == LOSE)||(GameOver(oState) == DRAW)){
-				if (GameOver(oState)== LOSE){
-				loss++;}
-				else {draw++;}
+		gameRes = GameOver(oState);
+		if ((gameRes == LOSE)||(gameRes == DRAW)) {
+				if (GameOver(oState)== LOSE) loss++;
+				else draw++;
 		    break;
 		}
-
 	}
 }
 
@@ -350,203 +318,159 @@ int game(int gridSize){ // only plays not update, output will be win or loose
 	while(1){
 
 		nextMove(oState, xState, GREEDY, PLAYER_X);
-		//
-		if ((GameOver(xState) == WIN)||(GameOver(xState) == LOSE)||(GameOver(xState) == DRAW)){
-		    // printMat(gridSize, xState.mat);
-		    return GameOver(xState);
-		}
+		int gameRes = GameOver(xState);
+		if ((gameRes == WIN)||(gameRes == DRAW)) return gameRes;
 
 		nextMove(xState, oState, EXPLORATORY, PLAYER_O);
-		//
-		if ((GameOver(oState) == WIN)||(GameOver(oState) == LOSE)||(GameOver(oState) == DRAW)){
-	        // printMat(gridSize, oState.mat);
-		    return GameOver(oState);
-		}
-
+		gameRes = GameOver(oState);
+		if ((gameRes == LOSE)||(gameRes == DRAW)) return gameRes;
 
 	}
-
 }
 
-//To Play against human
-/*****************************************************************************
-*******************************************************************************/
+// To Play against human
 void viewArena(int gridSize){
-	cout<<"Here is ur field :)"<<endl;
+	cout << "Here is ur field :)" << endl;
     for (int i = 0; i < gridSize; i++){
         for (int j = 0; j < gridSize; j++){
-            cout << i*gridSize+j+1;
-			if(j<gridSize-1){
-				 cout<< " | ";
-			}
+					cout << i*gridSize+j+1;
+					if(j < gridSize - 1) {
+				 		cout<< " |";
+					}
         }
-        if(i<gridSize-1) cout << "______________________" << endl;
+        if(i < gridSize - 1) cout << endl << "--------------------" << endl;
     }
-
-    cout << "-----------------------" << endl;
+    cout << endl<<"___________________" << endl;
 }
 
 
 
 void humanInput(state &S1, state &S2,int gridSize){
 	int move;
-	cout<<"choose ur move between 1 to " + to_string( gridSize)<<endl;
-	cout<<"Please dont choose the space occupied by X"<<endl;
-	cin>>move;
+	cout << "choose ur move between 1 to " + to_string(gridSize) << endl;
+	cout << "Please dont choose the space occupied by X" << endl;
+	cin >> move;
 	move--;
 	int sz = S1.mat.size();
-	int moveX=move/sz, moveY=move%sz;
-    equate(S1, S2);
-
-    S2.mat[moveX][moveY]= -1;
+	int moveX = move/sz, moveY = move%sz;
+  equate(S1, S2);
+	S2.mat[moveX][moveY]= -1;
   return;
 }
 
-
-
-int humanPlay(int gridSize){ // only plays not update, output will be win or loose
+void humanPlay(int gridSize){ // only plays not update, output will be win or loose
 
 	state oState(gridSize);
 	state xState(gridSize);
 	viewArena(gridSize);
+	int gameRes;
+
 	while(1){
-		cout<<"Learner's Move :"<<endl;
+		cout << "Learner's Move :" << endl;
 		nextMove(oState, xState, GREEDY, PLAYER_X);
+		gameRes = GameOver(xState);
 		printMat(gridSize, xState.mat);
-		//
-		if ((GameOver(xState) == WIN)){
-
-
-				cout<<"Learner Won"<<endl;// printMat(gridSize, xState.mat);
-		    return 0;
+		if ((gameRes == WIN)){
+				cout << "Learner Won" << endl; // printMat(gridSize, xState.mat);
+		    return;
 		}
-		if ((GameOver(xState) == DRAW)){
-
-
-				cout<<" It's a Draw"<<endl;// printMat(gridSize, xState.mat);
-		    return 0;
+		if ((gameRes == DRAW)){
+				cout << " It's a Draw" << endl; // printMat(gridSize, xState.mat);
+		    return;
 		}
-		if ((GameOver(xState) == LOSE)){
 
-
-				cout<<"Learner Lost,You Won"<<endl;// printMat(gridSize, xState.mat);
-		    return 0;
-		}
-		cout<<"Your Move :"<<endl;
-		humanInput(xState, oState,gridSize);
+		cout << "Your Move :" << endl;
+		humanInput(xState, oState, gridSize);
+		gameRes = GameOver(oState);
 		printMat(gridSize, oState.mat);
-		//
-		if ((GameOver(oState) == WIN)){
-
-
-				cout<<"Learner Lost,You won"<<endl;// printMat(gridSize, xState.mat);
-		    return 0;
+		if ((gameRes == LOSE)){
+				cout << "Learner Lost, You won" << endl; // printMat(gridSize, xState.mat);
+		    return;
 		}
-		if ((GameOver(oState) == DRAW)){
-
-
-				cout<<" It's a Draw"<<endl;// printMat(gridSize, xState.mat);
-		    return 0;
+		if ((gameRes == DRAW)){
+				cout << " It's a Draw" << endl; // printMat(gridSize, xState.mat);
+		    return;
 		}
-		if ((GameOver(oState) == LOSE)){
-
-
-				cout<<"Learner Won,You Lost"<<endl;// printMat(gridSize, xState.mat);
-		    return 0;
-		}
-
-
 	}
-
 }
 
-
-
-
-
-//Below snippet plotts the graphs
-/*************************************************************************************
-****************************************************************************************/
-//the below function gives destination for plotting image
+// Below snippet plotts the graphs
+// the below function gives destination for plotting image
 
 string plotFile(){
 	string epsilon, alpha,gridSize,trains,checkCount;
-	cout<<"What was the epsilon ?"<<endl;
+	cout << "What was the epsilon ?" << endl;
 	std::cin >> epsilon;
-	cout<<"Enter the Alpha again :-)"<<endl;
+	cout << "Enter the Alpha again :-)" << endl;
 	std::cin >> alpha;
-	cout<<"What was the size of tic-tac-toe?"<<endl;
+	cout << "What was the size of tic-tac-toe?" << endl;
 	std::cin >> gridSize;
-	cout<<"How many times you trained the Agent?"<<endl;
+	cout << "How many times you trained the Agent?" << endl;
 	std::cin >> trains;
 	std::cout << "Enter value of checkCount again :-)" << '\n';
 	std::cin >> checkCount;
 
-    string plotfile =
-    "Results/Grid-" + gridSize+ "/ Alpha-"+alpha+"_Epsilon-" +epsilon + "_Trainings-"
-    + trains + "CheckCount" +checkCount;
-
-    return plotfile;
+	string plotfile = "Results/Grid-" + gridSize+ "/ Alpha-"+alpha+"_Epsilon-" +epsilon + "_Trainings-" + trains + "CheckCount" +checkCount;
+	return plotfile;
 }
 
 //function to plot the graphs;
-void plotGraph(std::vector<float> Trainings,std::vector<float> winPercent,std::vector<float> drawPercent,std::vector<float> notLost,std::vector<float> lostPercent,string plotfileName){
-	string plotTitle="Percent vs Trains \n"+plotfileName;
+void plotGraph(std::vector<float> Trainings, std::vector<float> winPercent, std::vector<float> drawPercent, std::vector<float> notLost, std::vector<float> lostPercent, string plotfileName){
+	string plotTitle = "Percent vs Trains \n" + plotfileName;
 	using namespace plt;
-	named_plot("Draw",Trainings,drawPercent,"pink");
-	named_plot("Win",Trainings,winPercent,"r");
-	named_plot( "Win+Draw",Trainings,notLost,"b--");
-	named_plot("Lost",Trainings,lostPercent,"g--");
+	named_plot("Draw", Trainings, drawPercent, "pink");
+	named_plot("Win", Trainings, winPercent, "r");
+	named_plot( "Win+Draw", Trainings, notLost, "b--");
+	named_plot("Lost", Trainings, lostPercent, "g--");
 	legend();
 	ylim(-5, 105);
 	title(plotTitle);
 	save(plotfileName);
-	cout<<"The Graph is stored in file named : "<<plotfileName<<endl;
+	cout << "The Graph is stored in file named : " << plotfileName << endl;
 
 }
 
-/*************************************************************************************
-****************************************************************************************/
-
-
-
 int main(int argc, char **argv){
 
-
 	float epsilon, alpha;
-	int gridSize,trains;
+	int gridSize, trains;
 	float checkCount;
-	cout<<"What epsilon u want,choose any number between 0 to 1 : "<<endl
+	cout << "What epsilon u want,choose any number between 0 to 1 : " << endl
 	<<"But for better training prefer lesser value of epsilon"<<endl;
 	std::cin >> epsilon;
-	cout<<"What alpha u want,choose any number between 0 to 1"<<endl;
+	cout << "What alpha u want,choose any number between 0 to 1" << endl;
 	std::cin >> alpha;
-	cout<<"choose gridsize of tic-tac-toe"<<endl;
+	cout << "choose gridsize of tic-tac-toe" << endl;
 	std::cin >> gridSize;
-	cout<<"How many times do you want to train the Agent"<<endl;
+	cout << "How many times do you want to train the Agent" << endl;
 	std::cin >> trains;
-	cout<<"For how many games u want to calculate probablity after each Training i.e checkCount :"<<endl;
-	cout<<"If u thoose both no of trains and checkCount a high no,it may take a lot of time to calculate "<<endl;
+	cout << "For how many games u want to calculate probablity after each Training i.e checkCount :" << endl;
+	cout << "If u thoose both no of trains and checkCount a high no,it may take a lot of time to calculate " << endl;
 	std::cin >> checkCount;
-	cout<<"Please enter the data again that u entered now !"<<endl;
-
+	cout << "Please enter the data again that u entered now !" << endl;
 
 	std::vector<float> winPercent;
 	std::vector<float> drawPercent;
 	std::vector<float> Trainings;
 	std::vector<float> notLost;
 	std::vector<float> lostPercent;
-	string fileName=plotFile();
+
+	string fileName = plotFile();
+
 	string plotfileName = "Plot-" + fileName + ".png";
 	string textfileName = "Text-" + fileName + ".txt";
 	ofstream fout(textfileName.c_str());
-	fout<<"No of Trainings | Percentage";
+
+	fout << "No of Trainings | Percentage";
 
 	for (int i = 0; i < trains; i++) {
+
 		playGame(epsilon, alpha, gridSize);
-        float gamesWin = 0;
-      	float gamesDraw = 0;
-      	float gamesLose = 0;
+
+		float gamesWin = 0;
+		float gamesDraw = 0;
+		float gamesLose = 0;
+
 		for (int j = 0; j < int(checkCount); j++) {
 
 			int gameResult = game(gridSize);
@@ -555,34 +479,32 @@ int main(int argc, char **argv){
 			else if (gameResult == DRAW) gamesDraw++;
 
 		}
-        winPercent.push_back(100*gamesWin/checkCount);
+
+		winPercent.push_back(100*gamesWin/checkCount);
 		drawPercent.push_back(100*gamesDraw/checkCount);
-        notLost.push_back(100*( gamesDraw/checkCount + gamesWin/checkCount) );
-        lostPercent.push_back(100*gamesLose/checkCount);
+    notLost.push_back(100*( gamesDraw/checkCount + gamesWin/checkCount) );
+    lostPercent.push_back(100*gamesLose/checkCount);
 		Trainings.push_back(i);
-		int p=trains/1000;
-		if(i%p==0){
-			cout<<i<<"th training :=  Win Percent : "<<winPercent[i]<<" | Draw Percent :"<<drawPercent[i]<<" | Lost Percent :"<<lostPercent[i]<<endl;
-			fout<<i<<"th training :=  Win Percent :"<<winPercent[i]<<" | Draw Percent :"<<drawPercent[i]<<" | Lost Percent :"<<lostPercent[i]<<endl;
-		}
+
+		cout << i << "th training :=  Win Percent : " << winPercent[i] << " | Draw Percent :" << drawPercent[i] << " | Lost Percent :" << lostPercent[i] << endl;
+		fout << i << "th training :=  Win Percent :" << winPercent[i] << " | Draw Percent :" << drawPercent[i] << " | Lost Percent :" << lostPercent[i] << endl;
+
 	}
 
-
-	plotGraph(Trainings,winPercent,drawPercent,notLost,lostPercent,plotfileName);
+	plotGraph(Trainings, winPercent, drawPercent, notLost, lostPercent, plotfileName);
 	fout.close();
-	cout<<"And the stats are stored in file named : "<<textfileName<<endl;
-	cout<<"Wanna play with learner ??"<<endl;
-	int humanGameCount=1;
+	cout << "And the stats are stored in file named : " << textfileName << endl;
+	cout << "Wanna play with learner ??" << endl;
+	int humanGameCount = 1;
 	while(humanGameCount){
-		cout<<"Hey do u want to play with me :)"<<endl;
+		cout << "Hey do u want to play with me :)" << endl;
 		string humansChoice;
-		cout<<"Please reply with yes/no"<<endl;
-		cin>>humansChoice;
-		if(humansChoice=="yes") humanPlay(gridSize);
+		cout << "Please reply with yes/no" << endl;
+		cin >> humansChoice;
+		if(humansChoice == "yes") humanPlay(gridSize);
 		else humanGameCount--;
 	}
 
  return 0;
 
 }
-
