ec64245db4c82c1a85dc034932bc9749d5cf05bf
abhisaini <abhisni@iitk.ac.in>
2018-04-14 03:13:20 +0530

    fix

--- Weight_Learner.py
+++ Weight_Learner.py
@@ -18,16 +18,6 @@ EPSILON = 0.01
 WIN = 1
 DRAW = 2
 LOST = 4
-WEIGHTS = {
-    'win' : -10.0,
-    'fork' : 0.0,
-    'block' : 0.0,
-    'center' : 0.0,
-    'oppCprner' : 0.0,
-    'emptyCorner' : 0.0,
-    'emptySide' : 0.0,
-    'oppForkBlock' : 0.0
-    }
 def checkCombinations(Matrix,i,j) :
     new_Matrix = Matrix.copy()
     new_Matrix[i][j] = 1
@@ -41,7 +31,7 @@ def checkCombinations(Matrix,i,j) :
 def isFork(Matrix,i,j) :
     gridSize = np.shape(Matrix)[0]
     values = checkCombinations(Matrix,i,j)
-    # print(values)
+    # # # print(values)
     if values.count(gridSize - 1) > 1 :
         return float(1)
     return float(0)
@@ -49,7 +39,7 @@ def isFork(Matrix,i,j) :
 def isOppForkBlocked(Matrix,i,j) :
     gridSize = np.shape(Matrix)[0]
     values = checkCombinations(Matrix,i,j)
-    # print(values)
+    # # # print(values)
     if values.count(-1 * gridSize + 3) > 1 :
         return float(1)
     return float(0)
@@ -73,7 +63,7 @@ def isBlock(Matrix,i,j) :
 def isCenter(Matrix,i,j) :
     gridSize = np.shape(Matrix)[0]
     pm = int((gridSize % 2 -1 + gridSize) /2)
-    # print (pm)
+    # # # print (pm)
     if i == pm and j == pm :
         return float(1)
     return float(0)
@@ -82,14 +72,14 @@ def isOppCorner(Matrix,i,j) :
     gridSize = np.shape(Matrix)[0]
     if Matrix[gridSize - 1 - i][gridSize - 1 - i] == 1 :
         if (i == 0 or i == gridSize-1) and (j == 0 or j == gridSize-1) :
-            # print (1)
+            # # # print (1)
             return float(1)
     return float(0)
 
 def isEmptyCorner(Matrix,i,j) :
     gridSize = np.shape(Matrix)[0]
     if (i == 0 or i == gridSize-1) and (j == 0 or j == gridSize-1) :
-        # print (1)
+        # # # print (1)
         return float(1)
     return float(0)
 def isEmptySide(Matrix,i,j) :
@@ -117,7 +107,8 @@ def isDraw(Matrix) :
 
 
 # # # # # # # # # # # # # # # # # # # # # # # #
-def grossVal(Matrix,i,j) :
+def grossVal(Matrix,i,j,WEIGHTS) :
+    # # print (WEIGHTS)
     weights_num = np.array([[ WEIGHTS["win"], WEIGHTS["fork"], WEIGHTS["block"], WEIGHTS["oppForkBlock"], WEIGHTS["center"], WEIGHTS["oppCprner"], WEIGHTS["emptyCorner"], WEIGHTS["emptySide"] ]])
     classifiers_num = np.array([[ isWin(Matrix,i,j), isFork(Matrix,i,j), isBlock(Matrix,i,j), isOppForkBlocked(Matrix,i,j), isCenter(Matrix,i,j), isOppCorner(Matrix,i,j),isEmptyCorner(Matrix,i,j),isEmptySide(Matrix,i,j) ]])
     # classifiers_num.astype(float)
@@ -138,30 +129,30 @@ def randomMove(Matrix,player) :
         # Needed arguments can be put later
     return move_i, move_j
 
-def chooseBest(Matrix,possible_next_i,possible_next_j) :
+def chooseBest(Matrix,possible_next_i,possible_next_j,WEIGHTS) :
     values = []
     total_possiblities = len(possible_next_i)
     for x in range(total_possiblities) :
         tmp_i = possible_next_i[x]
         tmp_j = possible_next_j[x]
-        values.append(grossVal(Matrix,tmp_i,tmp_j))
+        values.append(grossVal(Matrix,tmp_i,tmp_j,WEIGHTS))
     max_val = max(values)
     indices_max = [i for i,x in enumerate(values) if x == max_val]
     move_tmp = indices_max[rand.randint(0, len(indices_max) - 1)]
     move_i = possible_next_i[move_tmp]
     move_j = possible_next_j[move_tmp]
     Matrix[move_i][move_j] = 1
-    print (values)
+    # # print (values)
     return move_i,move_j
 
-def greedyMove(Matrix) : 
+def greedyMove(Matrix,WEIGHTS) : 
     empty_i, empty_j = np.where(Matrix == 0) 
     possible_next_i =[]
     possible_next_j =[]
     if len(empty_i) >= 1 :
         for move_tmp in range(len(empty_i)) :
             # tmp = np.copy(Matrix)
-            # print(Matrix,tmp)
+            # # # print(Matrix,tmp)
             move_i = empty_i[move_tmp]
             move_j = empty_j[move_tmp]
             # tmp[move_i][move_j] = 1
@@ -170,50 +161,51 @@ def greedyMove(Matrix) :
             
         # possible_next_i = np.delete(possible_next,0, axis=0) 
         
-        return chooseBest(Matrix,possible_next_i,possible_next_j)   
+        return chooseBest(Matrix,possible_next_i,possible_next_j,WEIGHTS)   
             # tmp[move_i][move_j] = 0
-    print(possible_next_i,possible_next_j)
+    # # print(possible_next_i,possible_next_j)
     
-def nextMove(Matrix,player,policy) :
+def nextMove(Matrix,player,policy,WEIGHTS) :
     if player == PLAYER_X and policy == EXPLORATERY :
         return randomMove(Matrix,player)
     if player == PLAYER_O :
         return randomMove(Matrix,player)
     if player == PLAYER_X and policy == GREEDY :
-        return greedyMove(Matrix)
+        return greedyMove(Matrix,WEIGHTS)
 
-def gamePlay(Matrix,policy):
+def gamePlay(Matrix,policy,WEIGHTS):
     old_Matrix = Matrix.copy()
-    move_x_i, move_x_j = nextMove(Matrix,1,policy)
-    print("gross value : ", grossVal(old_Matrix,move_x_i,move_x_j))
+    move_x_i, move_x_j = nextMove(Matrix,1,policy, WEIGHTS)
+    # # print("gross value : ", grossVal(old_Matrix,move_x_i,move_x_j,WEIGHTS))
     Matrix[move_x_i][move_x_j] = 1
     if isWin(old_Matrix,move_x_i,move_x_j) == 1 :
         Matrix[move_x_i][move_x_j] = 1
-        print(Matrix)
-        print ("WIN")
+        # # print(Matrix)
+        # # print ("WIN")
         return WIN
     
     
     if isDraw(Matrix) == 1 :
         Matrix[move_x_i][move_x_j] = 1
-        print(Matrix)
-        print ("DRAW")
+        # # print(Matrix)
+        # # print ("DRAW")
         return DRAW
-    print(Matrix)
+    # # print(Matrix)
     old_Matrix = Matrix.copy()
-    move_o_i, move_o_j = nextMove(Matrix,-1,policy)
+    move_o_i, move_o_j = nextMove(Matrix,-1,policy,WEIGHTS)
     Matrix[move_o_i][move_o_j] = -1
     if isLost(old_Matrix,move_x_i,move_x_j) == 1 :
         Matrix[move_x_i][move_x_j] = 1
-        print(Matrix)
-        print ("LOST")
+        # # print(Matrix)
+        # # print ("LOST")
         return LOST
     
-    print(Matrix)
-    gamePlay(Matrix,policy)
+    # # print(Matrix)
+    return gamePlay(Matrix,policy,WEIGHTS)
 
-def games() :
-    
+def games(weight) :
+    WEIGHTS = weight
+    # # print(WEIGHTS)
     game_count = 1000
     win_count = 0
     lost_count = 0
@@ -231,57 +223,62 @@ def games() :
             [0, 0, 0],
             [0, 0, 0]
         ])
-        game_result = gamePlay(Matrix,policy)
+        game_result = gamePlay(Matrix,policy,WEIGHTS)
+        # print(game_result)
         if game_result == WIN :
+            # print("WIN")
             win_count += 1
         if game_result == DRAW :
+            # print("DRAW")
             draw_count += 1
         if game_result == LOST :
+            # print("LOST")
             lost_count += 1
     win_percent = win_count/game_count
     draw_percent = draw_count/game_count
     lost_percent = lost_count/game_count
-    return win_percent,draw_percent,lost_percent
+    res = [win_percent,draw_percent,lost_percent]
+    return res
 
 
 # any size of mat will be okay Here
 # numpy doesnt require to mention size
-mat = np.array([
-    [0, 0, 0],
-    [0, 0, 0],
-    [0, 0, 0]
-    ])
-gamePlay(mat,1)
-mat = np.array([
-    [0, -1, 0],
-    [0, 0, 1],
-    [-1, 1, 0]
-])
-print(mat,"\n")
-print("Is it forking at [1,1]", isFork(mat,1,1))
-print("And value for move [1,1] is : ", grossVal(mat,1,1))
+# mat = np.array([
+#     [0, 0, 0],
+#     [0, 0, 0],
+#     [0, 0, 0]
+#     ])
+# # gamePlay(mat,1)
+# mat = np.array([
+#     [0, -1, 0],
+#     [0, 0, 1],
+#     [-1, 1, 0]
+# ])
+# # # print(mat,"\n")
+# # # print("Is it forking at [1,1]", isFork(mat,1,1))
+# # # print("And value for move [1,1] is : ", grossVal(mat,1,1))
 
-print("Is it winning at [2,2]", isWin(mat,2,2))
-print("And value for move [2,2] is : ", grossVal(mat,2,2))
+# # # print("Is it winning at [2,2]", isWin(mat,2,2))
+# # # print("And value for move [2,2] is : ", grossVal(mat,2,2))
 
-print("Is it blocking at [2,2]", isBlock(mat,1,1))
-print("And value for move [2,2] is : ", grossVal(mat,2,2))
+# # # print("Is it blocking at [2,2]", isBlock(mat,1,1))
+# # # print("And value for move [2,2] is : ", grossVal(mat,2,2))
 
-print("Is it occupiying center at [1,1]", isCenter(mat,1,1))
-print("And value for move [1,1] is : ", grossVal(mat,1,1))
+# # # print("Is it occupiying center at [1,1]", isCenter(mat,1,1))
+# # # print("And value for move [1,1] is : ", grossVal(mat,1,1))
 
-print("Is it occupiying opp. corner at [2,2]", isOppCorner(mat,2,2))
-print("And value for move [2,2] is : ", grossVal(mat,2,2))
+# # # print("Is it occupiying opp. corner at [2,2]", isOppCorner(mat,2,2))
+# # # print("And value for move [2,2] is : ", grossVal(mat,2,2))
 
-print("Is it occupiying empty corner at [2,2]", isEmptyCorner(mat,2,2))
-print("And value for move [2,2] is : ", grossVal(mat,2,2))
+# # # print("Is it occupiying empty corner at [2,2]", isEmptyCorner(mat,2,2))
+# # # print("And value for move [2,2] is : ", grossVal(mat,2,2))
 
-print("Is it occupiying empty side at [2,1]", isEmptySide(mat,2,1))
-print("And value for move [2,1] is : ", grossVal(mat,2,1))
+# # # print("Is it occupiying empty side at [2,1]", isEmptySide(mat,2,1))
+# # # print("And value for move [2,1] is : ", grossVal(mat,2,1))
 
-print("Is it blocking opponent fork at [1,1]", isOppForkBlocked(mat,0,2), "\n")
-print(WEIGHTS)
-print(isLost(mat,2,2))
+# # # print("Is it blocking opponent fork at [1,1]", isOppForkBlocked(mat,0,2), "\n")
+# # # print(WEIGHTS)
+# # # print(isLost(mat,2,2))
 
 '''
 class Employee:
@@ -294,7 +291,7 @@ class Employee:
         Employee.empCount += 1
 
     def displayCount(self):
-        print ("Total Employee %d" % Employee.empCount)
+        # # print ("Total Employee %d" % Employee.empCount)
     def displayEmployee(self):
         return self.aa
 
@@ -303,8 +300,8 @@ emp1 = Employee("Zara", 2000)
 "This would create second object of Employee class"
 emp2 = Employee("Manni", 5000)
 emp1.displayEmployee()
-print (emp2.displayEmployee())
-print ("Total Employee %d" % Employee.empCount)
+# # print (emp2.displayEmployee())
+# # print ("Total Employee %d" % Employee.empCount)
 
 class State :
     def _init_(self) :
@@ -315,7 +312,7 @@ class State :
     def mat(self) :
         return self.Matrix
 S = State()
-print S.val()
+# # print S.val()
 
 0 y 0
 x 0 y
--- trainer.py
+++ trainer.py
@@ -1,33 +1,48 @@
 #!/usr/bin/python
 import numpy as np
 import random as rand
-import Weight_Learner.py
-
+import Weight_Learner as wl
+WEIGHTS = {
+    'win' : 0.0,
+    'fork' : 0.0,
+    'block' : 0.0,
+    'center' : 0.0,
+    'oppCprner' : 0.0,
+    'emptyCorner' : 0.0,
+    'emptySide' : 0.0,
+    'oppForkBlock' : 0.0
+    }
 def change_weight(delta_weight, model) :
-    WEIGHTS(model) = WEIGHTS(model) + delta_weight
+    WEIGHTS[model] = WEIGHTS[model] + delta_weight
+
 
 def trainer(value, iteration_count) :
     count = 0
     delta_weight = value
     while count < iteration_count :
-        prev_win_percent = games[0]
+        print("count : ",count)
+        print("WEIGHTS : ", WEIGHTS)
         change_weight(delta_weight, "win")
-        next_win_percent = games[0]
+        prev_win_percent = wl.games(WEIGHTS)[0]
+        print(prev_win_percent)
+        next_win_percent = wl.games(WEIGHTS)[0]
         delta_win_percent = next_win_percent - prev_win_percent
-
+        # change_weight(delta_win_percent/1000, "win")
+        print("Delta Percent : ", delta_win_percent)
         if delta_win_percent > 0 :
             count = count + 1
             continue
         else :
             delta_weight = -1*delta_weight
+            count = count + 1
             continue
-
     while count < iteration_count :
-        prev_win_percent = games[0]
+        print("count : ",count)
+        prev_win_percent = wl.games(WEIGHTS)[0]
         change_weight(delta_weight, "block")
-        next_win_percent = games[0]
+        next_win_percent = wl.games(WEIGHTS)[0]
         delta_win_percent = next_win_percent - prev_win_percent
-
+        # change_weight(delta_win_percent/1000, "block")
         if delta_win_percent > 0 :
             count = count + 1
             continue
@@ -36,11 +51,13 @@ def trainer(value, iteration_count) :
             continue
 
     while count < iteration_count :
-        prev_win_percent = games[0]
+        print("count : ",count)
+        prev_win_percent = wl.games(WEIGHTS)[0]
         change_weight(delta_weight, "fork")
-        next_win_percent = games[0]
-        delta_win_percent = next_win_percent - prev_win_percent
+        next_win_percent = wl.games(WEIGHTS)[0]
 
+        delta_win_percent = next_win_percent - prev_win_percent
+        # change_weight(delta_win_percent/1000, "fork")
         if delta_win_percent > 0 :
             count = count + 1
             continue
@@ -49,11 +66,11 @@ def trainer(value, iteration_count) :
             continue
 
     while count < iteration_count :
-        prev_win_percent = games[0]
+        prev_win_percent = wl.games(WEIGHTS)[0]
         change_weight(delta_weight, "oppForkBlock")
-        next_win_percent = games[0]
+        next_win_percent = wl.games(WEIGHTS)[0]
         delta_win_percent = next_win_percent - prev_win_percent
-
+        # change_weight(delta_win_percent/1000, "oppForkBlock")
         if delta_win_percent > 0 :
             count = count + 1
             continue
@@ -62,11 +79,11 @@ def trainer(value, iteration_count) :
             continue
 
     while count < iteration_count :
-        prev_win_percent = games[0]
+        prev_win_percent = wl.games(WEIGHTS)[0]
         change_weight(delta_weight, "center")
-        next_win_percent = games[0]
+        next_win_percent = wl.games(WEIGHTS)[0]
         delta_win_percent = next_win_percent - prev_win_percent
-
+        # change_weight(delta_win_percent/1000, "center")
         if delta_win_percent > 0 :
             count = count + 1
             continue
@@ -75,11 +92,11 @@ def trainer(value, iteration_count) :
             continue
 
     while count < iteration_count :
-        prev_win_percent = games[0]
+        prev_win_percent = wl.games(WEIGHTS)[0]
         change_weight(delta_weight, "oppCorner")
-        next_win_percent = games[0]
+        next_win_percent = wl.games(WEIGHTS)[0]
         delta_win_percent = next_win_percent - prev_win_percent
-
+        # change_weight(delta_win_percent/1000, "oppCorner")
         if delta_win_percent > 0 :
             count = count + 1
             continue
@@ -88,11 +105,11 @@ def trainer(value, iteration_count) :
             continue
 
     while count < iteration_count :
-        prev_win_percent = games[0]
+        prev_win_percent = wl.games(WEIGHTS)[0]
         change_weight(delta_weight, "emptyCorner")
-        next_win_percent = games[0]
+        next_win_percent = wl.games(WEIGHTS)[0]
         delta_win_percent = next_win_percent - prev_win_percent
-
+        # change_weight(delta_win_percent/1000, "emptyCorner")
         if delta_win_percent > 0 :
             count = count + 1
             continue
@@ -101,14 +118,17 @@ def trainer(value, iteration_count) :
             continue
 
     while count < iteration_count :
-        prev_win_percent = games[0]
+        prev_win_percent = wl.games(WEIGHTS)[0]
         change_weight(delta_weight, "emptySide")
-        next_win_percent = games[0]
+        next_win_percent = wl.games(WEIGHTS)[0]
         delta_win_percent = next_win_percent - prev_win_percent
-
+        # change_weight(delta_win_percent/1000, "emptySide")
         if delta_win_percent > 0 :
             count = count + 1
             continue
         else :
             delta_weight = -1*delta_weight
             continue
+count = 0
+trainer(0.0001,10000)
+print(WEIGHTS)
